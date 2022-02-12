1373d9f1756bca1a396b6fa1ad58385873df336d
Nathanael <ngreese7@ou.edu>
2022-02-12 13:44:47 -0600

    twillio workd now

--- Driver.py
+++ Driver.py
@@ -57,6 +57,8 @@ def translate(gcf_request=None):
 
 
 if __name__ == '__main__':
-    import os
-    app.run(debug=True, threaded=True, host='0.0.0.0',
-            port=int(os.environ.get('PORT', 8080)))
+    import Survey.py
+    Survey.complete()
+    #import os
+    #app.run(debug=True, threaded=True, host='0.0.0.0',
+            #port=int(os.environ.get('PORT', 8080)))
--- Survey.py
+++ Survey.py
@@ -1,9 +1,11 @@
 from twilio.rest import Client
 import toml
 
-
 account_sid = 'AC75a4dafecc596dab48029922f9ce4ac6'
-auth_token =
+config = toml.load("config.toml")
+tokendick = config.get("Twillio")
+auth_token = tokendick.get("key")
+
 client = Client(account_sid, auth_token)
 
 
@@ -16,8 +18,12 @@ def complete():
     message = client.messages.create(
         messaging_service_sid='MGc8bc55077da9aebfdc146d03c77e3aa2',
         body='Thank you for completing our survey! You will get your results shortly :)',
-        to=phone_num
+        to='+18177579731'
     )
     print(message.sid)
 
 
+def main():
+    complete()
+
+main()
