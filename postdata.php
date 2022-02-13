d269e19876a853012a5e951f7d9d0396ec4f8702
Nathanael <ngreese7@ou.edu>
2022-02-13 06:51:31 -0600

    sqlshit

--- postdata.php
+++ postdata.php
@@ -9,7 +9,8 @@ if (!$con) {
   die('Could not connect: ' . mysqli_error($con));
 }
 
-$sql="SELECT * FROM user WHERE id = '".$q."'";
+$sql="INSERT INTO user_data (name, phone, insta, q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12, q13, q14, q15, q16, q17, q18, q19, q20) VALUES ( 'Aaron Pierce', '+14053250566', '@aaron', 4, 5, 5, 4, 5, 1, 2, 4, 3, 1, 2, 5, 3, 5, 4, 2, 5, 1, 1, 1);
+;"
 $result = mysqli_query($con,$sql);
 
 
