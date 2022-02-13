a49d78f50165c2289dc3299f0ae7a0a9d58d5b63
Nathanael <ngreese7@ou.edu>
2022-02-13 06:42:00 -0600

    change get to post

--- postdata.php
+++ postdata.php
@@ -3,18 +3,6 @@ $q = $_GET['q'];
 
 echo "hmm"
 
-$Name = "Username:".$_POST['name']."
-    ";
-    $Phone = "Phone:".$_POST['phone']."
-    ";
-    $ig = "IG:".$_POST['ig']."
-    ";
-    $One = "One:".$_POST['one']."
-    ";
-    $Two = "Two:".$_POST['two']."
-    ";
-
-    $file=fopen("saved.txt", "w");
 //     fwrite($file, $Name);
 //     fwrite($file, $Phone);
 //     fwrite($file, $IG);
