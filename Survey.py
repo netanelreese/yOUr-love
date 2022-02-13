14719f2db470cacb674b7889a9dffdab7a605d99
root <root@your-love-vm.us-central1-a.c.emerald-mission-295821.internal>
2022-02-13 15:07:17 +0000

    UHHHHH

--- Survey.py
+++ Survey.py
@@ -15,11 +15,11 @@ user_db = mysql.connector.connect(
 print("here")
 
 account_sid = 'AC75a4dafecc596dab48029922f9ce4ac6'
-#config = toml.load("config.toml")
-#tokendic = config.get("Twillio")
-#auth_token = tokendic.get("key")
+config = toml.load("config.toml")
+tokendic = config.get("Twillio")
+auth_token = tokendic.get("key")
 
-#client = Client(account_sid, auth_token)
+client = Client(account_sid, auth_token)
 
 cursorObject = user_db.cursor()
 
@@ -73,10 +73,8 @@ def get_matches():
                 if num_same > matches[2][1]:
                     matches[2] = [get_name(user2), num_same, get_insta(user2)]
                     sort(matches)
-        #send_matches(matches_text(matches), get_phone_number(user1))
-        print(matches_text(matches))
-        print(get_phone_number(user1))
-        print(get_name(user1))
+        send_matches(matches_text(matches), get_phone_number(user1))
+    
 
 # returns the percent of number of same answers to 20 questions
 def num_same_answers(answers1, answers2):
