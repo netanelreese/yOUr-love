8314d44dae8fd1fa3a9e92ec29a139880399b65c
nickstoia1 <nickstoia1@ou.edu>
2022-02-12 18:15:38 -0600

    Updated methods with database shit

--- Survey.py
+++ Survey.py
@@ -9,8 +9,6 @@ user_db = mysql.connector.connect(
   database="your_love"
 )
 
-print(user_db)
-
 account_sid = 'AC75a4dafecc596dab48029922f9ce4ac6'
 config = toml.load("config.toml")
 tokendic = config.get("Twillio")
@@ -19,6 +17,7 @@ auth_token = tokendic.get("key")
 client = Client(account_sid, auth_token)
 
 cursorObject = user_db.cursor()
+
 # returns a persons answers (array of answers)
 def get_table():
     query = "SELECT * FROM user_data"
@@ -27,30 +26,48 @@ def get_table():
     for x in result:
         print(x)
     return result
+
+user_data = get_table()
+
 # returns the persons phone number
-def get_phone_number(name):
-    return ""
+def get_phone_number(user):
+    return user[1]
 
 # return the persons instagram handle
-def get_insta():
-    return ""
+def get_insta(user):
+    return user[2]
 
-# returns the persons top 3 matches
-def sort(matches):
-    pass
+# returns the persons name
+def get_name(user):
+    return user[0]
+
+# sorts the array with insertion sort
+def sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i][1]
+        j = i - 1
+        while j >= 0 and key < arr[j][1]:
+            arr[j + 1][1] = arr[j][1]
+            j -= 1
+        arr[j + 1][1] = key
+
 
 # TODO
 #
-def get_matches(name1):
-    matches = [['Blank', 0, 'insta'], ['Blank', 0, 'insta'], ['Blank', 0, 'insta']]  # Name, Percent
-    for name2 in persons:
-        answers1 = get_answers(name1)
-        answers2 = get_answers(name2)
-        num_same = num_same_answers(answers1, answers2)
-        if num_same > matches[2][1]:
-            matches[3] = [name2, num_same, get_insta()]
-            sort(matches)
-    return matches
+def get_answers(user):
+    return ""
+
+
+def get_matches():
+    for user1 in user_data:
+        matches = [['Blank', 0, 'insta'], ['Blank', 0, 'insta'], ['Blank', 0, 'insta']]  # Name, Percent
+        for user2 in user_data:
+            if user1 != user2:
+                num_same = num_same_answers(user1, user2)
+                if num_same > matches[2][1]:
+                    matches[3] = [get_name(user2), num_same, get_insta(user2)]
+                    sort(matches)
+        send_matches(matches_text(matches), get_phone_number(user1))
 
 # returns the percent of number of same answers to 20 questions
 def num_same_answers(answers1, answers2):
@@ -81,12 +98,11 @@ def matches_text(matches):
            matches[2][2]
 
 # texts the person their matches
-def send_matches(text):
+def send_matches(text, phone_number):
     message = client.messages.create(
         messaging_service_sid='MGc8bc55077da9aebfdc146d03c77e3aa2',
         body=text,
-        to='+18177579731'
+        to=phone_number
     )
     print(message.sid)
 
-get_table()
