ee8f485cff041d7501c299555f9274c055a8f3d3
nickstoia1 <nickstoia1@ou.edu>
2022-02-12 18:55:51 -0600

    Updated Sort

--- Survey.py
+++ Survey.py
@@ -43,13 +43,14 @@ def get_name(user):
 
 # sorts the array with insertion sort
 def sort(arr):
-    for i in range(1, len(arr)):
-        key = arr[i][1]
-        j = i - 1
-        while j >= 0 and key < arr[j][1]:
-            arr[j + 1] = arr[j]
-            j -= 1
-        arr[j + 1][1] = key
+    if arr[2][1]>arr[1][1]:
+        temp = arr[2]
+        arr[2] = arr[1]
+        arr[1] = temp
+    if arr[1][1]>arr[0][1]:
+        temp = arr[1]
+        arr[1] = arr[0]
+        arr[1] = temp
 
 
 # TODO
