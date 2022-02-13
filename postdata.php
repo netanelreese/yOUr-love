0afc1fdb1820514e1a14b32f5c79e0eb44c55198
Nathanael <ngreese7@ou.edu>
2022-02-13 07:27:19 -0600

    hmmm

--- postdata.php
+++ postdata.php
@@ -1,6 +1,9 @@
 <?php
 
-$con = new mysql_connect('34.122.13.220','root','Violentwinds212!');
+$con = mysql_connect('34.122.13.220','root','Violentwinds212!');
+
+$sql=("INSERT INTO user_data (name, phone, insta, q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12, q13, q14, q15, q16, q17, q18, q19, q20) VALUES ( 'Aaron Pierce', '+14053250566', '@aaron', 4, 5, 5, 4, 5, 1, 2, 4, 3, 1, 2, 5, 3, 5, 4, 2, 5, 1, 1, 1);
+;");
 
 if (!$con) {
   die('Could not connect: ' . mysqli_error($con));
@@ -10,18 +13,7 @@ $set = "USE your_love";
 
 mysql_query($set, $con);
 
-
-$sql="INSERT INTO user_data (name, phone, insta, q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12, q13, q14, q15, q16, q17, q18, q19, q20) VALUES ( 'Aaron Pierce', '+14053250566', '@aaron', 4, 5, 5, 4, 5, 1, 2, 4, 3, 1, 2, 5, 3, 5, 4, 2, 5, 1, 1, 1);
-;"
 mysql_query($sql, $con);
 
-mysql_close($con);
 
-echo "<script>console.log("Logged in")<script\>"
-//     fwrite($file, $Name);
-//     fwrite($file, $Phone);
-//     fwrite($file, $IG);
-//     fwrite($file, $One);
-//     fwrite($file, $Two);
-//     fclose($file);
 ?>
