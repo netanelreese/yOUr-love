b147d11ac2e80774e6f1d3f030cc7af329beb4f7
Nathanael <ngreese7@ou.edu>
2022-02-13 07:19:06 -0600

    remove syntax error

--- postdata.php
+++ postdata.php
@@ -13,7 +13,7 @@ mysql_query($set, $con);
 
 $sql="INSERT INTO user_data (name, phone, insta, q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12, q13, q14, q15, q16, q17, q18, q19, q20) VALUES ( 'Aaron Pierce', '+14053250566', '@aaron', 4, 5, 5, 4, 5, 1, 2, 4, 3, 1, 2, 5, 3, 5, 4, 2, 5, 1, 1, 1);
 ;"
-$result = mysql_query($sql, $con);
+mysql_query($sql, $con);
 
 mysql_close($con);
 
