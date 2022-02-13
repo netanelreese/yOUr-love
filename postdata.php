4742ff19671ac7ceae02dede5bb3780675a434e3
Nathanael <ngreese7@ou.edu>
2022-02-13 06:47:13 -0600

    uhhh

--- postdata.php
+++ postdata.php
@@ -5,6 +5,10 @@ echo "hmm"
 
 $con = mysqli_connect('34.122.13.220','root','Violentwinds212!','your_love');
 
+if (!$con) {
+  die('Could not connect: ' . mysqli_error($con));
+}
+
 echo "<script>console.log("Logged in")<script\>"
 //     fwrite($file, $Name);
 //     fwrite($file, $Phone);
