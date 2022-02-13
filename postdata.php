084e7f67a3173dec95da20afb0afa91280c04fff
Nathanael <ngreese7@ou.edu>
2022-02-13 06:44:34 -0600

    uhhh

--- postdata.php
+++ postdata.php
@@ -3,6 +3,9 @@ $q = $_GET['q'];
 
 echo "hmm"
 
+$con = mysqli_connect('34.122.13.220','root','Violentwinds212!','your_love');
+
+
 //     fwrite($file, $Name);
 //     fwrite($file, $Phone);
 //     fwrite($file, $IG);
